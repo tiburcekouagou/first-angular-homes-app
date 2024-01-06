be7ffd94d39b8b5702ffbf171769549c4b182fdd
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-01-06 11:52:23 +0100

    form created to apply for housings

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup ,ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { Housinglocation } from '../housinglocation';
@@ -6,7 +8,7 @@ import { Housinglocation } from '../housinglocation';
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
@@ -16,8 +18,23 @@ export class DetailsComponent {
   housingLocationId = -1;
   housingLocation: Housinglocation | undefined;
   
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
+  
   constructor() {
     this.housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
   }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
+  }
+
 }
