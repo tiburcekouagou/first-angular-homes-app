0f396b141ca0bb36b814c3fe681a62d408d1431c
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-01-06 10:24:33 +0100

    add input property to housingLocation comp

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Housinglocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
-
+@Input() housingLocation!: Housinglocation;
 }
