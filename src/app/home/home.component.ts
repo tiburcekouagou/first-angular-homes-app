bd4a8fba1b87bd3d6c6074808f4f0710ad3143db
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-01-06 10:07:39 +0100

    housing location comp added to home comp

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HousingLocationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
