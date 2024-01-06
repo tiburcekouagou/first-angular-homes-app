10d6d7961c572074f3154661395968470b12930c
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-01-06 11:34:58 +0100

    updated details comp

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject, Pipe } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { Housinglocation } from '../housinglocation';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
-
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(HousingService);
+  housingLocationId = -1;
+  housingLocation: Housinglocation | undefined;
+  
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
+  }
 }
